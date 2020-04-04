866432aaf65f7936b16f74965b8aee8f9668a9da
David <david@codersclan.net>
2020-04-04 19:11:25 +0300

    add _redirects

--- src/pages/Careers.js
+++ src/pages/Careers.js
@@ -26,7 +26,7 @@ class Careers extends Component {
                     </div>
                     <hr/>
                     <h2>Is your Cybersecurity company <strong>recruiting?</strong><br/>Submit who you’re looking for <a
-                        href={'https://airtable.com/shryWL3csxTNkq16M'}><strong>here</strong>.</a></h2>
+                        href={'https://airtable.com/shryWL3csxTNkq16M'} target="_blank"><strong>here</strong>.</a></h2>
                     <br/>
                     <p>List is constantly updating <span className="lds-hourglass"></span></p>
                     <div>
