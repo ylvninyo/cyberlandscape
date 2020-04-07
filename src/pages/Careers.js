35d9f2e1edd120157ca55b85786c4a4e2202f7fe
David <david@codersclan.net>
2020-04-07 11:47:32 +0300

    add meta module

--- src/pages/Careers.js
+++ src/pages/Careers.js
@@ -14,7 +14,7 @@ class Careers extends Component {
                     <title>Israeli Cybersecurity companies are hiring!</title>
                     <meta name="description" content="The Cybersecurity industry remains robust and resilient even as COVID-19 pummels the market. In this difficult time, let’s band together and create a resource for highly qualified professionals in the community looking for their next role!" />
                     <meta property="og:title" content="Israeli Cybersecurity companies are hiring!" />
-                    <meta property="og:image" content="https://www.cybermap.co/og-cybermap-careers.png" />
+                    <meta property="og:image" content="https://www.ylventures.com/wp-content/uploads/2020/04/og-cybermap-careers.png" />
                     <meta property="og:image:width" content="1200" />
                     <meta property="og:image:height" content="630" />
                 </MetaTags>
